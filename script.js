8ca0553d37898f24065e8219018a54ef2d9b21e7
solvman <solvman@gmail.com>
2024-02-13 09:01:16 -0600

    Toggle accordion content and update icons

--- script.js
+++ script.js
@@ -7,6 +7,14 @@ document.addEventListener("DOMContentLoaded", () => {
     const content = item.querySelector(".accordion-content");
 
     header.addEventListener("click", () => {
+      accordionItems.forEach((item) => {
+        if (item !== content.parentElement) {
+          item.querySelector(".accordion-content").classList.remove("active");
+          item.querySelector(".accordion-icon").classList.remove("icon-minus");
+          item.querySelector(".accordion-icon").classList.add("icon-plus");
+        }
+      });
+
       content.classList.toggle("active");
 
       if (content.classList.contains("active")) {
